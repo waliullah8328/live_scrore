9d1a2375ca2f303619a505a139e50aca04d2a43f
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2024-11-23 11:08:57 +0600

    Stream builder added.

--- lib/live_score/live_score.dart
+++ lib/live_score/live_score.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:live_scrore/model/cricket_score_model.dart';
 
@@ -35,6 +36,14 @@ class _LiveScoreState extends State<LiveScore> {
     _getScoreData();
   }
 
+  void extractData(QuerySnapshot<Map<String,dynamic>>? snapshot){
+    _cricketScore.clear();
+    for(DocumentSnapshot doc in snapshot?.docs?? []){
+      _cricketScore.add(CricketScoreModel.fromJson(doc.id ,doc.data() as Map<String,dynamic>));
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,33 +52,85 @@ class _LiveScoreState extends State<LiveScore> {
         title:const  Text("Live Scores",style: TextStyle(color: Colors.white),),
         centerTitle: true,
       ),
-      body: Visibility(
-        replacement: const Center(child: CircularProgressIndicator(),),
-        visible: _inProgress == false,
-        child: ListView.builder(
-          itemCount: _cricketScore.length,
-          physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-            final cricketScore = _cricketScore[index];
-              return ListTile(
+      body: Column(
+        children: [
 
-                leading: CircleAvatar(
-                  backgroundColor: _indicatorColor(cricketScore.isMatchRunning),
-                  radius: 8,
-                ),
-                title: Text(cricketScore.matchId),
-                subtitle: Text("Team 1: ${cricketScore.teamOneName} \n Team 2: ${cricketScore.teamTwoName}"),
-                trailing: Text("${cricketScore.teamOneScore}/ ${cricketScore.teamTwoScore}"),
+          // Manual featch Data and show the data
+      /*
+          Visibility(
+              replacement: const Center(child: CircularProgressIndicator(),),
+              visible: _inProgress == false,
+              child: ListView.builder(
+                itemCount: _cricketScore.length,
+                physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                  final cricketScore = _cricketScore[index];
+                    return ListTile(
 
-              );
-            },
-        ),
+                      leading: CircleAvatar(
+                        backgroundColor: _indicatorColor(cricketScore.isMatchRunning),
+                        radius: 8,
+                      ),
+                      title: Text(cricketScore.matchId),
+                      subtitle: Text("Team 1: ${cricketScore.teamOneName} \n Team 2: ${cricketScore.teamTwoName}"),
+                      trailing: Text("${cricketScore.teamOneScore}/ ${cricketScore.teamTwoScore}"),
+
+                    );
+                  },
+              ),
+            ),*/
+
+
+
+          //const SizedBox(height: 60,),
+           SizedBox(
+             height: 400,
+             child: StreamBuilder(
+                stream: FirebaseFirestore.instance.collection("cricket").snapshots(),
+                builder: (context,AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return const Center(child: CircularProgressIndicator(),);
+                  }
+                  if(snapshot.hasError){
+                    return Text(snapshot.error.toString());
+                  }
+                  if(snapshot.hasData){
+                    extractData(snapshot.data);
+                    return ListView.builder(
+                      itemCount: _cricketScore.length,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        final cricketScore = _cricketScore[index];
+                        return ListTile(
+
+                          leading: CircleAvatar(
+                            backgroundColor: _indicatorColor(cricketScore.isMatchRunning),
+                            radius: 8,
+                          ),
+                          title: Text(cricketScore.matchId),
+                          subtitle: Text("Team 1: ${cricketScore.teamOneName} \n Team 2: ${cricketScore.teamTwoName}\n Winner Team: ${cricketScore.winnerTeam}"),
+                          trailing: Text("${cricketScore.teamOneScore}/ ${cricketScore.teamTwoScore}"),
+
+                        );
+                      },
+                    );
+
+                  }
+                  return const SizedBox();
+
+                }
+              ),
+           ),
+
+
+
+        ],
       ),
 
     );
   }
   Color _indicatorColor(bool isMatchRunning){
-    return isMatchRunning? Colors.green:Colors.grey;
+    return isMatchRunning? Colors.red:Colors.yellow;
 
   }
 }
